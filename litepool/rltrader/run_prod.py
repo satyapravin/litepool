76f82da2b1d544cc2be8bf20974c8374bbf88dde
Pravin <pravin.bezwada@gmail.com>
2025-02-10 20:53:43 +0800

    deribit python script

---
+++ litepool/rltrader/run_prod.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import gymnasium
+from typing import Optional
+import numpy as np
+import torch as th
+import torch.nn as nn
+from packaging import version
+from sb3_contrib import RecurrentPPO
+from stable_baselines3 import SAC, PPO
+from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import VecEnvWrapper, VecMonitor, VecNormalize
+from stable_baselines3.common.vec_env.base_vec_env import (
+  VecEnvObs,
+  VecEnvStepReturn,
+)
+from stable_baselines3.common.torch_layers import create_mlp
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from stable_baselines3.sac.policies import SACPolicy
+from stable_baselines3.common.callbacks import BaseCallback
+
+import litepool
+from litepool.python.protocol import LitePool
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from typing import Optional, Type
+from gymnasium import spaces
+
+device = torch.device("cuda")
+
+
+
+class ResetHiddenStateCallback(BaseCallback):
+    def __init__(self, verbose=0):
+        super().__init__(verbose)
+
+    def _on_step(self) -> bool:
+        dones = self.locals["dones"]  
+        for env_index, done in enumerate(dones):
+            if done:  
+                self.model.actor.features_extractor.reset_hidden_state_for_env(env_index)
+        return True
+
+
+class VecAdapter(VecEnvWrapper):
+  def __init__(self, venv: LitePool):
+    venv.num_envs = venv.spec.config.num_envs
+    super().__init__(venv=venv)
+    self.mid_prices = []
+    self.balances = []
+    self.leverages = []
+    self.trades = []
+    self.fees = []
+    self.buys = []
+    self.sells = []
+    self.upnl = []
+    self.steps = 0
+    self.header = True
+
+  def step_async(self, actions: np.ndarray) -> None:
+      self.actions = actions 
+      self.venv.send(self.actions)
+
+  def reset(self) -> VecEnvObs:
+      self.steps = 0
+      return self.venv.reset()[0]
+
+  def seed(self, seed: Optional[int] = None) -> None:
+     if seed is not None:
+          self.venv.seed(seed) 
+
+  def step_wait(self) -> VecEnvStepReturn:
+      obs, rewards, terms, truncs, info_dict = self.venv.recv()
+
+     
+      if (np.isnan(obs).any() or np.isinf(obs).any()):
+          print("NaN in OBS...................")
+
+      if (np.isnan(rewards).any() or np.isinf(rewards).any()):
+          print("NaN in REWARDS...................")
+          print(rewards)
+
+      dones = terms + truncs
+      infos = []
+      for i in range(len(info_dict["env_id"])):
+          infos.append({
+              key: info_dict[key][i]
+              for key in info_dict.keys()
+              if isinstance(info_dict[key], np.ndarray)
+          })
+
+          if infos[i]["env_id"] == 0:
+              self.mid_prices.append(infos[i]['mid_price'])
+              self.balances.append(infos[i]['balance'])
+              self.upnl.append(infos[i]['unrealized_pnl'])
+              self.leverages.append(infos[i]['leverage'])
+              self.trades.append(infos[i]['trade_count'])
+              self.fees.append(infos[i]['fees'])
+              self.buys.append(infos[i]['buy_amount'])
+              self.sells.append(infos[i]['sell_amount']) 
+           
+          if dones[i]:
+              infos[i]["terminal_observation"] = obs[i]
+              obs[i] = self.venv.reset(np.array([i]))[0]
+
+          if self.steps % 1800 == 0 or dones[i]:
+              if infos[i]["env_id"] == 0:
+                  d = {"mid": self.mid_prices, "balance": self.balances, "upnl" : self.upnl, 
+                       "leverage": self.leverages, "trades": self.trades, "fees": self.fees,
+                       "buy_amount": self.buys, "sell_amount": self.sells } 
+                  df = pd.DataFrame(d)
+
+                  if self.header:
+                       df.to_csv("temp.csv", header=self.header, index=False) 
+                       self.header=False
+                  else:
+                       df.to_csv("temp.csv", mode='a', header=False, index=False)
+
+                  self.mid_prices = []
+                  self.balances = []
+                  self.upnl = []
+                  self.leverages = []
+                  self.trades = []
+                  self.fees = []
+                  self.buys = []
+                  self.sells = [] 
+                  self.header = False
+                  self.header = dones[i]
+              print("env_id ", i,  " steps ", self.steps, 'balance = ',infos[i]['balance'], "  unreal = ", infos[i]['unrealized_pnl'], 
+                    " real = ", infos[i]['realized_pnl'], '    drawdown = ', infos[i]['drawdown'], '     fees = ', infos[i]['fees'], 
+                    ' leverage = ', infos[i]['leverage'])
+      self.steps += 1
+      return obs, rewards, dones, infos
+
+import os
+if os.path.exists('temp.csv'):
+    os.remove('temp.csv')
+
+env = litepool.make("RlTrader-v0", env_type="gymnasium", 
+                          num_envs=1, batch_size=1,
+                          num_threads=1,
+                          is_prod=True,
+                          is_inverse_instr=True,
+                          api_key="",
+                          api_secret="",
+                          symbol="BTC-PERPETUAL",
+                          tick_size=0.5,
+                          min_amount=10,
+                          maker_fee=0.0,
+                          taker_fee=0.0005,
+                          foldername="./testfiles/", 
+                          balance=0.001,
+                          start=1,
+                          max=72000001,
+                          depth=20)
+env.spec.id = 'RlTrader-v0'
+
+env = VecAdapter(env)
+env = VecNormalize(env, norm_obs=True, norm_reward=True)
+env = VecMonitor(env)
+
+import os
+
+if os.path.exists("sac_rltrader.zip"):
+    model = SAC.load("sac_rltrader", env)
+    print("saved model loaded")
+
+    obs = env.reset()
+    done = False
+
+    counter = 0
+
+    while not done:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, dones, info = env.step(action)
+
+        if dones.any():
+            obs = env.reset()
+            done = True
+
+        counter += 1
+
+    env.close()
